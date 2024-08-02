2bb371a948281c23c8ecd77e1b1ea56b3d71f64e
Aye Mon Yadanar Aung <ayemonyadanar.aung2006@gmail.com>
2024-08-02 22:00:29 +0630

    feat: Driver, Vehicle, Ride Booking, Ride Order

---
+++ rentals/rentals/doctype/vehicle/patches/set_title.py
@@ -0,0 +1,10 @@
+import frappe
+# Needs optimization
+def execute():
+    vehicles = frappe.db.get_all("Vehicle", pluck="name")
+    for v in vehicles:
+        vehicle = frappe.get_doc("Vehicle", v)
+        vehicle.set_title()
+        vehicle.save()
+    
+    frappe.db.commit()
