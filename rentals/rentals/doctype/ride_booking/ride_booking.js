c6bcb350fcbd2d232d6dacfedb6896efa7454b72
Aye Mon Yadanar Aung <ayemonyadanar.aung2006@gmail.com>
2024-08-02 22:01:51 +0630

    feat: Ride Booking Web Actions

--- rentals/rentals/doctype/ride_booking/ride_booking.js
+++ rentals/rentals/doctype/ride_booking/ride_booking.js
@@ -1,8 +1,31 @@
 // Copyright (c) 2024, AMYA and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Ride Booking", {
-// 	refresh(frm) {
+frappe.ui.form.on("Ride Booking", {
+	refresh(frm) {
+		console.log(`refresh (${frm.doc.doctype})`);
+	},
+	rate(frm) {
+		frm.trigger("update_total_amount");
+	},
+	update_total_amount(frm) {
+		let total_distance = 0;
+		for (let item of frm.doc.items) {
+			total_distance += item.distance
+		}
+		const amount = frm.doc.rate * total_distance;
+		frm.set_value("total_amount", amount);
+	}
+});
 
-// 	},
-// });
+frappe.ui.form.on('Ride Booking Item', {
+	refresh(frm) {
+		// your code here
+	},
+	items_remove(frm) {
+		frm.trigger("update_total_amount");
+	},
+	distance(frm, cdt, cdn) {
+		frm.trigger("update_total_amount");
+	}
+})
