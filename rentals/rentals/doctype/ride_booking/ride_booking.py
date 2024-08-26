6c0957809fce50644562405c77b7691c4d49cece
Aye Mon Yadanar Aung <ayemonyadanar.aung2006@gmail.com>
2024-08-26 22:39:26 +0630

    feat: Get Default Rate from Rentals Settings

--- rentals/rentals/doctype/ride_booking/ride_booking.py
+++ rentals/rentals/doctype/ride_booking/ride_booking.py
@@ -7,7 +7,7 @@ from frappe.model.document import Document
 class RideBooking(Document):
 	def validate(self):
 		if not self.rate:
-			frappe.throw("Please provide a rate.")
+			self.rate = frappe.db.get_single_value("Rentals Settings", "standard_rate")
 		
 		total_distance = 0
 		for item in self.items:
